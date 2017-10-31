8b0f3f12a74e7e4893307c5891e748b8d8c2380b
Dhiorgenes <dhiorgenessantana@gmail.com>
2017-10-31 09:27:08 -0300

    Arrumando o banco

--- mysql_1fase(teste)/banco.sql
+++ mysql_1fase(teste)/banco.sql
@@ -1,27 +1,23 @@
-
-
 create table users(
-	id int unsigned auto_increment,
-	nome varchar(45) not null,
-	funcao varchar(45) not null,
-	cpf varchar(11) unique not null,
-	username varchar(45) not null,
-	password varchar(45) not null,
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(45) NOT NULL,
+	funcao VARCHAR(45) NOT NULL,
+	cpf VARCHAR(11) unique NOT NULL,
+	username VARCHAR(45) NOT NULL,
+	password VARCHAR(45) NOT NULL,
 	created DATETIME DEFAULT NULL,
-	modified DATETIME DEFAULT NULL,
-	primary key(id)	
+	modified DATETIME DEFAULT NULL
 );
 
 create table produtos(
-	id int auto_increment,
-	descricao varchar(100) not null,
-	unidade_de_medida varchar(2) not null,
-	quantidade_por_caixa float(4) not null,
-	embalagem varchar(45) not null,
-	dimensoes float(4) not null,
-	preco_por_caixa float(5) not null,
-	demanda_mensal float(5) not null,
-	produto_user_cad int,
-	primary key(id),
-	constraint produto_user_fk foreign key produtos(produto_user_cad) references users(id)
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	descricao VARCHAR(100) NOT NULL,
+	unidade_de_medida VARCHAR(2) NOT NULL,
+	quantidade_por_caixa FLOAT(4) NOT NULL,
+	embalagem VARCHAR(45) NOT NULL,
+	dimensoes FLOAT(4) NOT NULL,
+	preco_por_caixa FLOAT(5) NOT NULL,
+	demanda_mensal FLOAT(5) NOT NULL,
+	produto_user_cad INT,
+	CONSTRAINT produto_user_fk FOREIGN KEY (produto_user_cad) REFERENCES users(id)
 );
