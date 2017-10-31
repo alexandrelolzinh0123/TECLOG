60a2e04252e2902c5ef830da05ccf84a5f3483b8
Amandaalmeida12 <amandaisabel237@gmail.com>
2017-10-31 10:44:26 -0300

    Banco versao 2.1

--- mysql_1fase(teste)/banco.sql
+++ mysql_1fase(teste)/banco.sql
@@ -1,26 +1,29 @@
-
-create table users(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	nome VARCHAR(45) NOT NULL,
-	funcao VARCHAR(45) NOT NULL,
-	cpf VARCHAR(11) unique NOT NULL,
-	username VARCHAR(45) NOT NULL,
-	password VARCHAR(45) NOT NULL,
-	created DATETIME DEFAULT NULL,
-	modified DATETIME DEFAULT NULL
+CREATE TABLE products(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	descricao VARCHAR(80) NOT NULL,
+	quantidade_atual INTEGER NOT NULL,
+	created DATETIME not null,
+	modified DATETIME not null,
+	constraint id_pk primary key(id)
 );
 
-create table produtos(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	descricao VARCHAR(100) NOT NULL,
-	unidade_de_medida VARCHAR(2) NOT NULL,
-	quantidade_por_caixa FLOAT(4) NOT NULL,
-	embalagem VARCHAR(45) NOT NULL,
-	dimensoes FLOAT(4) NOT NULL,
-	preco_por_caixa FLOAT(5) NOT NULL,
-	demanda_mensal FLOAT(5) NOT NULL,
-	produto_user_cad INT,
-	rua VARCHAR(1) NOT NULL,
-	posicao INT NOT NULL,
-	CONSTRAINT produto_user_fk FOREIGN KEY (produto_user_cad) REFERENCES users(id)
+CREATE TABLE lots(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	quantidade_entrada INTEGER NOT NULL,
+	entrada DATETIME NOT NULL,
+	validade DATETIME NOT NULL,
+	quantidade_saida INTEGER NOT NULL,
+	product_id INTEGER NOT NULL,
+	cpf_funcionario VARCHAR(12) NOT NULL,
+	constraint pk_id primary key(id),
+	constraint product_id_fk foreign key(product_id) references products(id),
+	constraint cpf_fk foreign key(cpf_funcionario) references users(cpf)
+);
+
+CREATE TABLE users(
+	name VARCHAR(40) NOT NULL,
+	cpf VARCHAR(12) NOT NULL,
+	password VARCHAR(70) NOT NULL,
+	username VARCHAR(20) NOT NULL,
+	constraint cpf_pk primary key(cpf)
 );
